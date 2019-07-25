7dda9242b12efdb277a84cb0f7cf99325782332d
NJ261 <nirav9033@gmail.com>
2019-07-25 20:33:36 -0300

    DjangoSite: mapnikApp removed

--- DjangoSite/DjangoSite/urls.py
+++ DjangoSite/DjangoSite/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('foliumApp/', include('foliumApp.urls')),
-    path('mapnikApp/', include('mapnikApp.urls')),
 ]
